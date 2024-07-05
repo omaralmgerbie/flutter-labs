f331a55ce13597322ed36571d66441df8cc1fa0e
KautherAbulgasem <167922967+KautherAbulgasem@users.noreply.github.com>
2024-07-06 01:16:39 +0200

    Update home_page.dart

--- assignment2/named_routes/code/home_page.dart
+++ assignment2/named_routes/code/home_page.dart
@@ -1,55 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_project2/providers/todo_provider.dart';
-import 'package:flutter_project2/models/todo.dart';
-import 'task_page.dart';
+import 'task_provider.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final taskProvider = Provider.of<TaskProvider>(context);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Todo List'),
-      ),
-      body: Consumer<TodoProvider>(
-        builder: (context, todoProvider, child) {
-          return ListView.builder(
-            itemCount: todoProvider.todos.length,
-            itemBuilder: (context, index) {
-              final todo = todoProvider.todos[index];
-              return ListTile(
-                title: Text(todo.title),
-                trailing: Checkbox(
-                  value: todo.isCompleted,
-                  onChanged: (bool? value) {
-                    todoProvider.toggleTodoStatus(todo);
-                  },
-                ),
-                onLongPress: () {
-                  todoProvider.deleteTodo(todo);
-                },
-                onTap: () {
-                  Navigator.pushNamed(
-                    context,
-                    '/task',
-                    arguments: TaskArguments(todo: todo),
-                  );
-                },
-              );
+      appBar: AppBar(title: Text('Todo App')),
+      body: ListView.builder(
+        itemCount: taskProvider.tasks.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(taskProvider.tasks[index]),
+            trailing: IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                taskProvider.deleteTask(index);
+              },
+            ),
+            onTap: () {
+              Navigator.pushNamed(
+                context,
+                '/update',
+                arguments: taskProvider.tasks[index],
+              ).then((updatedTask) {
+                if (updatedTask != null) {
+                  taskProvider.updateTask(index, updatedTask as String);
+                }
+              });
             },
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushNamed(
-            context,
-            '/task',
-            arguments: TaskArguments(todo: null),
-          );
+        onPressed: () async {
+          final result = await Navigator.pushNamed(context, '/create');
+          if (result != null) {
+            taskProvider.addTask(result as String);
+          }
         },
         child: Icon(Icons.add),
       ),
     );
   }
 }
+
