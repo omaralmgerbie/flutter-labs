4920700d440dfb972e7188c5391d55a7a940c4b6
KautherAbulgasem <167922967+KautherAbulgasem@users.noreply.github.com>
2024-07-06 01:17:51 +0200

    Update not_found_page.dart

--- assignment2/named_routes/code/not_found_page.dart
+++ assignment2/named_routes/code/not_found_page.dart
@@ -4,12 +4,8 @@ class NotFoundPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Page Not Found'),
-      ),
-      body: Center(
-        child: Text('404 - Page Not Found'),
-      ),
+      appBar: AppBar(title: Text('Page Not Found')),
+      body: Center(child: Text('404 - Page Not Found')),
     );
   }
 }
