a8b3f67c82146223923db371b628e03671b3509b
KautherAbulgasem <167922967+KautherAbulgasem@users.noreply.github.com>
2024-07-06 01:16:14 +0200

    Update main.dart

--- assignment2/named_routes/code/main.dart
+++ assignment2/named_routes/code/main.dart
@@ -1,39 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_project2/pages/task_provider.dart';
 import 'package:flutter_project2/pages/home_page.dart';
-import 'package:flutter_project2/pages/task_page.dart';
+import 'package:flutter_project2/pages/create_task_page.dart';
+import 'package:flutter_project2/pages/update_task_page.dart';
 import 'package:flutter_project2/pages/not_found_page.dart';
-import 'package:flutter_project2/providers/todo_provider.dart';
 
 void main() {
-  runApp(TodoApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => TaskProvider(),
+      child: TodoApp(),
+    ),
+  );
 }
 
 class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => TodoProvider(),
-      child: MaterialApp(
-        title: 'Todo App',
-        initialRoute: '/',
-        onGenerateRoute: (settings) {
-          switch (settings.name) {
-            case '/':
-              return MaterialPageRoute(builder: (context) => HomePage());
-            case '/task':
-              final TaskArguments args = settings.arguments as TaskArguments;
-              return MaterialPageRoute(
-                builder: (context) => TaskPage(taskArguments: args),
-              );
-            default:
-              return MaterialPageRoute(builder: (context) => NotFoundPage());
-          }
-        },
-        onUnknownRoute: (settings) {
-          return MaterialPageRoute(builder: (context) => NotFoundPage());
-        },
-      ),
+    return MaterialApp(
+      initialRoute: '/',
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/':
+            return MaterialPageRoute(builder: (_) => HomePage());
+          case '/create':
+            return MaterialPageRoute(builder: (_) => CreateTaskPage());
+          case '/update':
+            return MaterialPageRoute(
+                builder: (_) =>
+                    UpdateTaskPage(task: settings.arguments as String));
+          default:
+            return MaterialPageRoute(builder: (_) => NotFoundPage());
+        }
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (_) => NotFoundPage());
+      },
     );
   }
 }
